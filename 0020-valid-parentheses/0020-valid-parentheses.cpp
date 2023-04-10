7f6508449b38e257b1ca1dd61cf9213a7769ad5c
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-04-11 00:38:15 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (12.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char> st;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ')' || s[i] == '}' || s[i] == ']') {
+                if (st.empty()) {
+                    return false;
+                }
+                if (s[i] == ')' && st.top() != '(') {
+                    return false;
+                }
+                if (s[i] == '}' && st.top() != '{') {
+                    return false;
+                }
+                if (s[i] == ']' && st.top() != '[') {
+                    return false;
+                }
+                st.pop();
+            } else {
+                st.push(s[i]);
+            }
+        }
+        
+        if (!st.empty()) {
+            return false;
+        }
+        return true;
+    }
+};
