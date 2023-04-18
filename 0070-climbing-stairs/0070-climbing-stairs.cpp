b0843d453110c89906a3aae2b20d3fdd639ecd72
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-04-18 19:33:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (50.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-   int climbStairs(int n) {
-        int p1= 1;
-        int p2 =1;
-        for(int i=2; i<=n;i++)
+  int climbStairs(int n) {
+       if(n==1) return 1;
+        int p1=1;
+        int p2=2;
+        for(int i=3;i<=n;i++)
         {
             int dp= p1+p2;
-            p1= p2;
+            p1=p2;
             p2=dp;
         }
         return p2;
