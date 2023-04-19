be044eaf454de7bda4174ecc2d6f3f04fbf7dfff
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-04-20 01:34:41 +0530

    Time: 7 ms (21.06%), Space: 6.6 MB (87.37%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -3,15 +3,24 @@ public:
     int longestPalindrome(string s) {
         int ans = 0, odd = 0;
         map<char, int> mp;
-        for(auto c: s) mp[c]++;
-        for(auto x: mp) {
-            if(x.second % 2 == 0) ans += x.second;
-            else {
-                ans += x.second - 1;
-                odd = 1;
-            }
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
         }
-        if(odd) ans++;
-        return ans;
+        for(auto it: mp)
+        {
+            if(it.second %2 ==0)
+                ans+=it.second;
+            else
+            {
+                ans+=it.second-1;
+                odd=1;
+            }
+                
+        }
+        if(odd)
+            return ans+1;
+        else
+            return ans;
     }
 };
