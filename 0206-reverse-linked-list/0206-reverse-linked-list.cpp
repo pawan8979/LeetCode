cbcf784cb59967dc8672cb58a6ce5853f0edf1cb
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-04-20 18:29:11 +0530

    Time: 3 ms (95.18%), Space: 8.3 MB (85.92%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,33 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-     ListNode* reverseList(ListNode* head) {
-        if(head==NULL)
-        {
-            return head;
+    ListNode* reverseList(ListNode* head) {
+        // Special case...
+        if(head == NULL || head->next == NULL)  return head;
+        // Initialize prev pointer as the head...
+        ListNode* prev = head;
+        // Initialize curr pointer as the next pointer of prev...
+        ListNode* curr = prev->next;
+        // Initialize next of head pointer as NULL...
+        head->next = NULL;
+        // Run a loop till curr and prev points to NULL...
+        while(prev != NULL && curr != NULL){
+            // Initialize next pointer as the next pointer of curr...
+            ListNode* next = curr->next;
+            // Now assign the prev pointer to curr’s next pointer.
+            curr->next = prev;
+            // Assign curr to prev, next to curr...
+            prev = curr;
+            curr = next;
         }
-        ListNode *current=head;
-            ListNode *prev=NULL;
-            ListNode *next=NULL;
-            
-            while(current!=NULL)
-            {
-                next=current->next;
-                current->next=prev;
-                prev=current;
-                current=next;
-            }
-            head=prev;
-        
-        return head;
+        return prev;    // Return the prev pointer to get the reverse linked list...
     }
 };
