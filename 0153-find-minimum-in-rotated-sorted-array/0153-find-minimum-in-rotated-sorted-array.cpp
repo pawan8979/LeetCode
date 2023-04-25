21ba35c3d4c84194e1a4b197f0138feccd864a32
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-04-26 01:19:27 +0530

    Time: 4 ms (56.15%), Space: 10.2 MB (78.89%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -11,40 +11,60 @@
 //     }
 // };
 
+// Better Approach
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int low = 0;
+//         int high = nums.size() - 1;
+//     // If the array is not rotated
+//         if (nums[low] <= nums[high]) {
+//             return nums[low];
+//         }
 
-class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
-    // If the array is not rotated
-        if (nums[low] <= nums[high]) {
-            return nums[low];
-        }
+//         // Binary search
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
 
-        // Binary search
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            // Check if mid is the minimum element
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                return nums[mid];
-            }
+//             // Check if mid is the minimum element
+//             if (mid > 0 && nums[mid] < nums[mid - 1]) {
+//                 return nums[mid];
+//             }
             
 
-            // If the right half is sorted, the minimum element
-            // must be in the left half
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            }
+//             // If the right half is sorted, the minimum element
+//             // must be in the left half
+//             if (nums[mid] > nums[high]) {
+//                 low = mid + 1;
+//             }
 
-            // If the left half is sorted, the minimum element
-            // must be in the right half
-            else {
-                high = mid - 1;
-            }
-        }
-    // If no minimum element is found, return -1
-    return -1;
+//             // If the left half is sorted, the minimum element
+//             // must be in the right half
+//             else {
+//                 high = mid - 1;
+//             }
+//         }
+//     // If no minimum element is found, return -1
+//     return -1;
+//     }
+// };
+
+//Best Approach
+
+class Solution {
+ public:
+  int findMin(vector<int>& nums) {
+    int l = 0;
+    int r = nums.size() - 1;
+
+    while (l < r) {
+        int m = (l + r) / 2;
+      if (nums[m] < nums[r])
+        r = m;
+      else
+        l = m + 1;
     }
+
+    return nums[l];
+  }
 };
