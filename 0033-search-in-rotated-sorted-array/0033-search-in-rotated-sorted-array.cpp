a6bc1f69784da6ad4abca5bdeb0a4cfb34f0c1dc
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-04-27 01:25:25 +0530

    Time: 3 ms (80.01%), Space: 11.1 MB (81.49%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,38 @@
+// Linear search
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
-public:
+public:  
     int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==target)
-            {
-                return i;
-            }
+        int size=nums.size();
+       int s=0,e=size-1,m=0;
+        while(s<=e){
+                m=s+(e-s)/2;
+                if(nums[m]==target) return m;
+                if(nums[m]>=nums[s]){
+                if(nums[m]>=target && nums[s]<=target) e=m-1;
+                else s=m+1;}
+                else{
+                    if(nums[m]<=target && nums[e]>=target) s=m+1;
+                    else e=m-1;
+                }
         }
         return -1;
+        
+
+        
     }
 };
