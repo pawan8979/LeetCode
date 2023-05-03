6a19fcfbf9c14bfcca23431380c05fa0961bbedb
pawan8979 <107505388+pawan8979@users.noreply.github.com>
2023-05-03 19:19:20 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (90.09%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<int> countBits(int n) {
+    //TC O(n)
+    vector<int> countBits(int n) {  
+        // n+1 as we are going to count from 0 to n
+        vector<int> v(n+1);
         
-        int i=0;
-        vector<int> v;
-        while(i<=n)
-        {
-            int count=0;
-            int num=i;
-             while (num!=0) 
-            { 
-              num &= (num-1) ; 
-              count++; 
-            } 
-            v.push_back(count);
-            i++;
-        }
+        // v[0] will be 0 beacuse 0 has count of set bit is 0;
+        v[0] = 0;
+        
+        // we can compute current set bit count using previous count
+        // as x/2 in O(1) time
+        
+        // i%2 will be 0 for even number ans 1 for odd number
+        
+        for(int i = 1; i<=n; ++i)
+            v[i] = v[i/2] + i%2;
+        
         return v;
     }
 };
