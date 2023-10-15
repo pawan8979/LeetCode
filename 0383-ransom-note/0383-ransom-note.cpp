360dbbe952576e06aa3c4b49a82b7a8261103a12
Pawan Negi <107505388+pawan8979@users.noreply.github.com>
2023-10-15 21:53:02 +0530

    Time: 13 ms (73.83%), Space: 9.3 MB (8.03%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,16 @@
+//OPTIMIZED
 class Solution {
 public:
-   bool canConstruct(string ransomNote, string magazine) {
-       unordered_map<char, int> mp;
-        for(int i=0; i<magazine.length();i++)
-        {
-            mp[magazine[i]]++;
+  bool canConstruct(string ransomNote, string magazine) {
+        int dict[26]={0};
+        for(auto letter:magazine){
+            dict[letter-97]++;
         }
-        for(int i=0; i<ransomNote.length();i++)
-        {
-            if(mp[ransomNote[i]]==0)
+        for(auto letter:ransomNote){
+            if(dict[letter-97]==0)
                 return false;
-            mp[ransomNote[i]]--;
+            else
+                dict[letter-97]--;
         }
         return true;
     }
