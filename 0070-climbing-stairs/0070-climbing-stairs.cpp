47966ec9db2c2591be9d87f6a8c574ad713892ff
Pawan Negi <107505388+pawan8979@users.noreply.github.com>
2023-10-16 21:54:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (53.96%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-  int climbStairs(int n) {
-       if(n==1) return 1;
-        int p1=1;
-        int p2=2;
+    int climbStairs(int n) {
+        if(n==1) return 1;
+        int dp[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
         for(int i=3;i<=n;i++)
         {
-            int dp= p1+p2;
-            p1=p2;
-            p2=dp;
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return p2;
+        return dp[n];
     }
 };
