9af9d96292bfcf3de79c54b88e180c4ffab26b0d
Pawan Negi <107505388+pawan8979@users.noreply.github.com>
2023-10-27 21:48:17 +0530

    Time: 12 ms (73.37%), Space: 17.2 MB (40.52%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-         int ans=0;
-	    for(auto i:nums)
-	        ans^=i;
-	    return ans;
+        int ans= 0;
+        for(auto value:nums)
+        {
+            ans = value^ans;
+        }
+        return ans;
     }
 };
