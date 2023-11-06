7c2d13c5141ecb382b4eb04d7c848ffbe3a683f6
Pawan Negi <107505388+pawan8979@users.noreply.github.com>
2023-11-07 00:40:07 +0530

    Time: 326 ms (18.10%), Space: 194.5 MB (11.77%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    //taking two pointers i and j
-    //At the beginning both i and j with point at first index
+   vector<int> maxSlidingWindow(vector<int>& nums, int k) {
             int i = 0;
             int j = 0;
             int size = nums.size();
-    //creating a list to store all possible max elements temporarily 
             list<int>l;
             vector<int>ans;
             while(j<size){
-    //removing all elements from list which are lesser than the element we are going to add
                 while(!l.empty() && l.back()<nums[j]){
                     l.pop_back();
                 }
-    //pushing new element into the list
                 l.push_back(nums[j]);
                 if(j-i+1<k){
-    //taking j pointer to end of the first window
                     j++;
                 }
                 else if(j-i+1==k){
-    //if window size reached then push the front element of list into the answer vector
                   ans.push_back(l.front());
-    //if front of list is equal to front element of sliding window 
-    //then we need to remove that element from list 
-    //because it is exiting out of current window 
-    // so we have no use of that element
-    //hence we remove that element
                   if(l.front()==nums[i]){
                       l.pop_front();
                   }
-    //sliding the window by one index
                   i++;
                   j++;
                 }
             }
-    //returning the answer vector consisting of all max elements
             return ans;
     }
 };
