5cfa9d70d3d14f6c0fde484b9cc91f772ef83b94
Pawan Negi <107505388+pawan8979@users.noreply.github.com>
2023-12-04 18:43:54 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (45.95%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,12 +2,14 @@ class Solution {
 private:
     void helper(int start, vector<int> nums, vector<vector<int>>& ans, vector<int> output)
     {
-        ans.push_back(output);
+        ans.push_back(output); //[]
         for(int i=start;i<nums.size();i++)
         {
-            output.push_back(nums[i]);
+            output.push_back(nums[i]); //[1]
+            //now below call will find all the possible subset with 1
             helper(i+1, nums, ans, output);
-            output.pop_back();
+            //now after finding possible subset backtrack
+            output.pop_back();//[]
         }
     }
 public:
